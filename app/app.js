2bc9fdef0361e6e26ddc2f4985ea1a66563fcc80
Walter Chavarria <walter.chavarria@agilysys.com>
2017-03-02 15:56:59 -0600

    Basic Scaffolding

---
+++ app/app.js
@@ -0,0 +1,5 @@
+
+
+// require('./favicon.ico'); // Tell webpack to load favicon.ico
+import './styles/base.css';
+import './styles/index.css';
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'inline-source-map',
+  context: path.join(__dirname, '/app'),
+
+  entry: {
+    app: './app.js'
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/'
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,'/app/index.html')
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: { presets: ['es2015'] },
+        exclude: [/node_modules/]
+      },
+      { 
+        test: /\.vue$/, 
+        loader: 'vue-loader' 
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader?sourceMap', 'sass-loader']
+        //use: ['style-loader', 'css-loader?sourceMap?minimize', 'sass-loader']
+      }
+    ]
+  }
+}
